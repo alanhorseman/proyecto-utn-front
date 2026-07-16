74eb9453ba71c65bd918357a14f4c4aac9cfc4fb
alan barbera <aedev86@gmail.com>
2026-07-16 00:46:28 -0300

    homeScreen

--- src/Screens/HomeScreen/HomeScreen.jsx
+++ src/Screens/HomeScreen/HomeScreen.jsx
@@ -1,11 +1,20 @@
 import { useContext } from "react"
 import { AuthContext } from "../../context/AuthContext"
-
+import { useNavigate } from "react-router"
 
 export const HomeScreen = () => {
 
-  const {isLogged} = useContext(AuthContext)
+  const {isLogged, logout} = useContext(AuthContext)
+  const navigate = useNavigate()
+
+  function handleLogout(){
+    logout()
+    navigate('/login')
+  }
   return (
-    <div>HomeScreen</div>
+    <div>
+      <h1>HomeScreen</h1>
+      <button onClick={handleLogout}>Cerrar sesion</button>
+    </div>
   )
 }
